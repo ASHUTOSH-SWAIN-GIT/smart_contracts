397eb2536f236790b0cf10c2c24c19b4fa020797
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-06-05 08:23:28 +0530

    finish first contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,43 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
-    account_info::{AccountInfo,next_account_info},
-    entrypoint::ProgramResult,entrypoint,
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
     pubkey::Pubkey,
-    msg
 };
+#[derive(BorshDeserialize, BorshSerialize)]
+enum Instruction {
+    Increment(u32),
+    Decrement(u32),
+}
+#[derive(BorshDeserialize, BorshSerialize)]
+struct Counter {
+    count: u32,
+}
 
-entrpoint!(counter_contract);
-
+entrypoint!(counter_contract);
 
 pub fn counter_contract(
-    program_id:&Pubkey,
-    accounts:&[AccountInfo],
-    instruction_data:&[u8]
-) -> ProgramResult{
-     Ok(())
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let acc = next_account_info(&mut accounts.iter())?;
+    let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
+
+    let instruction = Instruction::try_from_slice(instruction_data)?;
+    match instruction {
+        Instruction::Increment(value) => {
+            msg!("Incrementing counter by {}", value);
+            counter_data.count += value;
+        }
+        Instruction::Decrement(value) => {
+            msg!("Decrementing counter by {}", value);
+            counter_data.count -= value;
+        }
+    }
+    counter_data.serialize(&mut &mut acc.data.borrow_mut()[..])?;
+    msg!("Counter updated: {}", counter_data.count);
+    Ok(())
 }
